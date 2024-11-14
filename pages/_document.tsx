1397e747900ec0ef1cb8d9110584d20769fe7f7f
epidote-stone <epidotestone@gmail.com>
2024-11-14 02:26:28 +0000

    feat: Add Twitter meta tags for social media sharing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,11 @@ export default function Document() {
       <meta property="og:type" content="website" />
       <meta property="og:image" content="https://r2.wakttu.kr/og-image.png" />
       <meta property="og:description" content="Meta Tag Site description" />
+
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Meta Tag Site" />
+      <meta name="twitter:image" content="https://r2.wakttu.kr/og-image.png" />
+      <meta name="twitter:description" content="summary_large_image" />
 
       <title>Meta Tag</title>
 
